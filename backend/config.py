e64c7874ddbde6e51d49d0d03f4a7c317a0b28ba
YarkinAkyosun <yarkinak01@gmail.com>
2024-05-06 11:39:53 +0300

    removed unnecsary secret key

--- backend/config.py
+++ backend/config.py
@@ -3,6 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY')
+    #SECRET_KEY = os.getenv('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
