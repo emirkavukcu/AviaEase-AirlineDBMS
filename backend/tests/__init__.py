aa5c4205cec292b2c41699e2b7d36bc7c794d5fa
Emir Kavukcu <emir.kavukcu@gmail.com>
2024-05-24 00:41:51 +0300

    Save current changes before pulling

--- backend/tests/__init__.py
+++
@@ -1,9 +0,0 @@
-from .test_airports import *
-from .test_cabin_crew import *
-from .test_flight_views import *
-from .test_flights import *
-from .test_models import *
-from .test_passengers import *
-from .test_pilots import *
-from .test_roster import *
-
