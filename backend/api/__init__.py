79e289b68a9c9c6d712ab25aceb86044e8f4888b
emirkavukcu <136182336+emirkavukcu@users.noreply.github.com>
2024-05-24 01:26:56 +0300

    Update __init__.py, remove /api prefix

--- backend/api/__init__.py
+++ backend/api/__init__.py
@@ -10,10 +10,10 @@ from .flight_views import flight_views
 from .airports import airports
 
 def register_blueprints(app):
-    app.register_blueprint(flights, url_prefix='/api')
-    app.register_blueprint(roster, url_prefix='/api')
-    app.register_blueprint(passengers, url_prefix='/api')
-    app.register_blueprint(cabin_crew, url_prefix='/api')
-    app.register_blueprint(pilots, url_prefix='/api')
-    app.register_blueprint(flight_views, url_prefix='/api')
-    app.register_blueprint(airports, url_prefix='/api')
+    app.register_blueprint(flights, url_prefix='/')
+    app.register_blueprint(roster, url_prefix='/')
+    app.register_blueprint(passengers, url_prefix='/')
+    app.register_blueprint(cabin_crew, url_prefix='/')
+    app.register_blueprint(pilots, url_prefix='/')
+    app.register_blueprint(flight_views, url_prefix='/')
+    app.register_blueprint(airports, url_prefix='/')
